729e3cf6efd1ddc908191ae60cd8c7d275ff39ad
ramgopalnbh <ramgopalnbh@gmail.com>
2021-04-21 10:12:51 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'home',
+    component : HomeComponent
+  },
+  
+  {
+    path : 'category',
+    component : CategoryComponent
+  },
+  
+  {
+    path: 'product',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'productCatalog'`, () => {
+  it(`should have as title 'product-catalog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('productCatalog');
+    expect(app.title).toEqual('product-catalog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('productCatalog app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('product-catalog app is running!');
   });
 });
