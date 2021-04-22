18a5df1a24f9114ba081b1c80db9f1b6272b1043
RamGopal <56515607+ramgopalnbh@users.noreply.github.com>
2021-04-22 13:53:24 +0530

    initial commit

---
+++ Product-Catalog-main/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,33 @@
+﻿using Assignment2.Entities;
+using System;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome to Product-Catalog, Please Select Option\n");
+            Console.WriteLine("\t\ta. Category");
+            Console.WriteLine("\t\tb. Product");
+            Console.WriteLine("\t\tc. Exit App!");
+
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            switch(ch)
+            {
+                case 'a':
+                    CategoryOperation.CategoryOperationMenu();
+                    break;
+                case 'b':
+                    ProductOperation.ProductOperationMenu();
+                    break;
+                case 'c':
+                    break;
+                default:
+                    Console.WriteLine("Sorry!!, Invalid Selection");
+                    break;
+            }
+        }
+    }
+}
